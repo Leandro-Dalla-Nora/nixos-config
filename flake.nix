0c202ca2f70815c2c45078af73f9dc51fd001f4e
Leandro Dalla Nora <workdallanora@gmail.com>
2024-01-16 17:29:46 -0300

    Versão funcional do flake.nix

--- flake.nix
+++ flake.nix
@@ -11,8 +11,43 @@
         specialArgs = { 
           inherit inputs; 
         };
-        # modules = map (import ./modules/nixos + "/${f}") (builtins.listDirectory ./modules/nixos);
-        modules = builtins.concatStringsSep " " builtins.readDir ./modules/nixos;
+        modules = [
+          ./modules/nixos/nixpkgs.nix
+          ./modules/nixos/utils.nix
+          ./modules/nixos/auto-upgrade.nix
+          ./modules/nixos/configuration.nix
+          ./modules/nixos/terminal-utils.nix
+          ./modules/nixos/networking.nix
+          ./modules/nixos/keyMap.nix
+          ./modules/nixos/bootloader.nix
+          ./modules/nixos/wayland.nix
+          ./modules/nixos/sound.nix
+          ./modules/nixos/programming.nix
+          ./modules/nixos/xserver.nix
+          ./modules/nixos/virtualisation.nix
+          ./modules/nixos/users.nix
+          ./modules/nixos/bluetooth.nix
+          ./modules/nixos/info-fetchers.nix
+          ./modules/nixos/security-services.nix
+          ./modules/nixos/environment-variables.nix
+          ./modules/nixos/mac-randomize.nix
+          ./modules/nixos/printing.nix
+          ./modules/nixos/theme.nix
+          ./modules/nixos/dns.nix
+          ./modules/nixos/nix-settings.nix
+          ./modules/nixos/screen.nix
+          ./modules/nixos/gnome.nix
+          ./modules/nixos/fonts.nix
+          ./modules/nixos/usb.nix
+          ./modules/nixos/vpn.nix
+          ./modules/nixos/gc.nix
+          ./modules/nixos/time.nix
+          ./modules/nixos/internationalisation.nix
+          ./modules/nixos/swap.nix
+          ./modules/nixos/services.nix
+          ./modules/nixos/open-ssh.nix
+          ./modules/nixos/hardware-configuration.nix
+        ];
       };
   };
 }
