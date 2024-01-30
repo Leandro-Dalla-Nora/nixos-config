77a1d8f2ed18ce4e6c4c04ffdb49b69e280fbf50
Leandro Dalla Nora <workdallanora@gmail.com>
2024-01-30 21:20:30 +0000

    Configuração da waybar

--- modules/user/wayland/waybar.nix
+++ modules/user/wayland/waybar.nix
@@ -34,10 +34,6 @@ custom ? {
       margin-right= 0;
       modules-left= [
         "custom/launcher" 
-        "custom/playerctl#backward" 
-        "custom/playerctl#play" 
-        "custom/playerctl#foward" 
-        "custom/playerlabel"
       ];
       modules-center= [
         "cava#left"
@@ -72,88 +68,7 @@ custom ? {
           sort-by-number= true;
         };
       };
-      "cava#left" = {
-        framerate = 60;
-        autosens = 1;
-        bars = 18;
-        lower_cutoff_freq = 50;
-        higher_cutoff_freq = 10000;
-        method = "pipewire";
-        source = "auto";
-        stereo = true;
-        reverse = false;
-        bar_delimiter = 0;
-        monstercat = false;
-        waves = false;
-        input_delay = 2;
-        format-icons = [ 
-          "<span foreground='#${custom.primary_accent}'>▁</span>" 
-          "<span foreground='#${custom.primary_accent}'>▂</span>" 
-          "<span foreground='#${custom.primary_accent}'>▃</span>" 
-          "<span foreground='#${custom.primary_accent}'>▄</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▅</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▆</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▇</span>" 
-          "<span foreground='#${custom.secondary_accent}'>█</span>" 
-        ];
-      };
-      "cava#right" = {
-        framerate = 60;
-        autosens = 1;
-        bars = 18;
-        lower_cutoff_freq = 50;
-        higher_cutoff_freq = 10000;
-        method = "pipewire";
-        source = "auto";
-        stereo = true;
-        reverse = false;
-        bar_delimiter = 0;
-        monstercat = false;
-        waves = false;
-        input_delay = 2;
-        format-icons = [ 
-          "<span foreground='#${custom.primary_accent}'>▁</span>" 
-          "<span foreground='#${custom.primary_accent}'>▂</span>" 
-          "<span foreground='#${custom.primary_accent}'>▃</span>" 
-          "<span foreground='#${custom.primary_accent}'>▄</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▅</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▆</span>" 
-          "<span foreground='#${custom.secondary_accent}'>▇</span>" 
-          "<span foreground='#${custom.secondary_accent}'>█</span>" 
-        ];
-      };
-      "custom/playerctl#backward"= {
-        format= "󰙣 "; 
-        on-click= "playerctl previous";
-        on-scroll-up = "playerctl volume .05+";
-        on-scroll-down = "playerctl volume .05-";
-      };
-      "custom/playerctl#play"= {
-        format= "{icon}";
-        return-type= "json";
-        exec= "playerctl -a metadata --format '{\"text\": \"{{artist}} - {{markup_escape(title)}}\", \"tooltip\": \"{{playerName}} : {{markup_escape(title)}}\", \"alt\": \"{{status}}\", \"class\": \"{{status}}\"}' -F";
-        on-click= "playerctl play-pause";
-        on-scroll-up = "playerctl volume .05+";
-        on-scroll-down = "playerctl volume .05-";
-        format-icons= {
-          Playing = "<span>󰏥 </span>";
-          Paused = "<span> </span>";
-          Stopped = "<span> </span>";
-        };
-      };
-      "custom/playerctl#foward"= {
-        format= "󰙡 ";
-        on-click= "playerctl next";
-        on-scroll-up = "playerctl volume .05+";
-        on-scroll-down = "playerctl volume .05-";
-      };
-      "custom/playerlabel"= {
-        format= "<span>󰎈 {} 󰎈</span>";
-        return-type= "json";
-        max-length= 40;
-        exec = "playerctl -a metadata --format '{\"text\": \"{{artist}} - {{markup_escape(title)}}\", \"tooltip\": \"{{playerName}} : {{markup_escape(title)}}\", \"alt\": \"{{status}}\", \"class\": \"{{status}}\"}' -F";
-        on-click= "";
-      };
+      
       battery= {
         states= {
           good= 95;
@@ -223,19 +138,6 @@ custom ? {
             window#waybar {
                 background: ${custom.palette.primary_background_rgba};
             }
-
-            #cava.left, #cava.right {
-                background: #${custom.palette.tertiary_background_hex};
-                margin: 5px; 
-                padding: 8px 16px;
-                color: #${custom.primary_accent};
-            }
-            #cava.left {
-                border-radius: 24px 10px 24px 10px;
-            }
-            #cava.right {
-                border-radius: 10px 24px 10px 24px;
-            }
             #workspaces {
                 background: #${custom.palette.tertiary_background_hex};
                 margin: 5px 5px;
@@ -269,8 +171,7 @@ custom ? {
                 background-size: 400% 400%;
             }
 
-            #tray, #pulseaudio, #network, #battery,
-            #custom-playerctl.backward, #custom-playerctl.play, #custom-playerctl.foward{
+            #tray, #pulseaudio, #network, #battery{
                 background: #${custom.palette.tertiary_background_hex};
                 font-weight: bold;
                 margin: 5px 0px;
@@ -298,39 +199,7 @@ custom ? {
                 padding: 0px 35px 0px 15px;
                 font-size: 28px;
             }
-
-            #custom-playerctl.backward, #custom-playerctl.play, #custom-playerctl.foward {
-                background: #${custom.palette.tertiary_background_hex};
-                font-size: 22px;
-            }
-            #custom-playerctl.backward:hover, #custom-playerctl.play:hover, #custom-playerctl.foward:hover{
-                color: #${custom.tertiary_accent};
-            }
-            #custom-playerctl.backward {
-                color: #${custom.primary_accent};
-                border-radius: 24px 0px 0px 10px;
-                padding-left: 16px;
-                margin-left: 7px;
-            }
-            #custom-playerctl.play {
-                color: #${custom.secondary_accent};
-                padding: 0 5px;
-            }
-            #custom-playerctl.foward {
-                color: #${custom.primary_accent};
-                border-radius: 0px 10px 24px 0px;
-                padding-right: 12px;
-                margin-right: 7px
-            }
-            #custom-playerlabel {
-                background: #${custom.palette.tertiary_background_hex};
-                color: #${custom.tertiary_accent};
-                padding: 0 20px;
-                border-radius: 24px 10px 24px 10px;
-                margin: 5px 0;
-                font-weight: bold;
-            }
-            #window{
+	    #window{
                 background: #${custom.palette.tertiary_background_hex};
                 padding-left: 15px;
                 padding-right: 15px;
